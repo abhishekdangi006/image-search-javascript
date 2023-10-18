cd25818e1cec9d9ddd0d23b83b346535e277d076
Abhishek Dangi <abhishekdangi006@gmail.com>
2023-10-18 17:44:00 +0530

    all the styles has been completed

---
+++ script.js
@@ -0,0 +1,13 @@
+const accessKey = "LU998HjnVZvz6udqOwvqcuuKfnP_jvLNEpW_PcrXqWA";
+
+const formE1 = document.querySelector("form");
+const inputE1 = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+
+let inputData = "";
+let page = 1;
+
+function searchImages(){
+    
+}
